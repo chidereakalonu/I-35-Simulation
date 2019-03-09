750ed766c9dad73c4ae1fffdfa00acbe05e76981
Davin Huckleberry <dhuck@tinycact.us>
2019-03-09 15:14:52 -0600

    highway test

--- tests/test_highway.cpp
+++ tests/test_highway.cpp
@@ -9,12 +9,21 @@ TEST_CASE("Testing default constructor", "Highway") {
     int length = default.get_length();
 
     REQUIRE(name == "whatever he chooses as a default");
-    REQUIRE(lanes == 3);
-    REQUIRE(width == 400);
-    REQUIRE(length == 400);
+    REQUIRE(lanes == MIN_LANES);
+    REQUIRE(width == MIN_WIDTH);
+    REQUIRE(length == MIN_LENGTH);
 
 }
-// 
-// TEST_CASE("Testing Constructor", "Highway") {
-//     Highway explicit;
-// }
+
+TEST_CASE("Testing Constructor", "Highway") {
+    Highway explicit(4, 500, 1000, "Name");
+    std::string name = default.get_name();
+    int lanes = default.get_lanes();
+    int width = default.get_width();
+    int length = default.get_length();
+
+    REQUIRE(name == "Name");
+    REQUIRE(lanes == 4);
+    REQUIRE(width == 500);
+    REQUIRE(length == 1000);
+}
