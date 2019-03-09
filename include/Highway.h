cc4a8c9b07fec6d311668341384dea4dcabe2d40
Davin Huckleberry <dhuck@tinycact.us>
2019-03-09 14:26:04 -0600

    restructured highway header

--- include/Highway.h
+++ include/Highway.h
@@ -3,12 +3,22 @@
 #include <string>
 
 class Highway {
-    public:
-	std::string name;	// name of this highway
-        int lanes;		// number of lanes 
-        int width;		// width of each lane in pixels
-        int length;		// length of each lane segment
-
+private:
+    std::string name;	// name of this highway
+    int lanes;		// number of lanes
+    int width;		// width of each lane in pixels
+    int length;		// length of each lane segment
+public:
+    // constructors
 	Highway();		// default constructor
+    Highway(int lane, int wid, int len, std::string name);
+
+    // accessors
+    void get_name();
+    void get_lanes();
+    void get_width();
+    void get_length();
+
+    // mgmt methods
 	void draw();		// display the highway
 };
