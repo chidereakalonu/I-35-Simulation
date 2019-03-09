4c1a2a1d8ca67725086ba2ce11089488aa04380a
Davin Huckleberry <dhuck@tinycact.us>
2019-03-09 15:24:17 -0600

    draw in glut

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,12 @@
 #include "Highway.h"
 int main() {
     std::cout << "Hello, World!";
+
+    graphicsSetup(argc, argv);      // initialize the graphics system
+    glutDisplayFunc(drawScene);     // tell GLUT what function draws the scene
+    glutIdleFunc(animate);          // Move objects when animating
+    glutKeyboardFunc(handleKey);    // set up the "q" key to quit
+    glutMainLoop();                 // GLUT will control the action
+
     return 0;
 }
